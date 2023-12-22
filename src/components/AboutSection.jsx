a2c7d50b0031a6712c726fa4f9af91a4fd8561a6
[Yuvenal Njoroge] <[yuvenal.njoroge@gmail.com]>
2023-12-22 19:45:25 +0300

    Add new components: AboutSection, FAQSection, Header, HeadlineSection, and TestimonialsSlider

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,11 @@
+// AboutSection component
+
+const AboutSection = () => {
+  return (
+    <section className="about-section bg-lime-700 text-white p-3">
+      <h2>About Section</h2>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/components/TestimonialsSlider.jsx
@@ -0,0 +1,11 @@
+// TestimonialsSlider component
+
+const TestimonialsSlider = () => {
+  return (
+    <section className="testimonials-slider bg-blue-700 text-white p-3">
+      <h2>Testimonials Slider</h2>
+    </section>
+  );
+};
+
+export default TestimonialsSlider;
