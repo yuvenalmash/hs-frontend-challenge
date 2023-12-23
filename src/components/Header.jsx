f2696aa964973fff007a3e12a4920b78280737f5
[Yuvenal Njoroge] <[yuvenal.njoroge@gmail.com]>
2023-12-23 03:26:23 +0300

    Update header styling in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,17 @@
 
 const Header = () => {
   return (
-    <header className="App-header bg-cyan-950 text-white p-3">
-      <h1>My React App!</h1>
+    <header className="App-header sm:h-[130.08px] text-">
+      <nav className="flex bg-[#685DC5] py-3 px-4 sm:px-14 sm:py-5 text-white items-center justify-between">
+        <div className="flex gap-2 items-center">
+          <p className="font-bold text-lg">
+            HARBOUR.SPACE
+          </p>
+          <p className="text-xs">
+            /INTERACTION DESIGN
+          </p>
+        </div>
+      </nav>
     </header>
   );
 };
