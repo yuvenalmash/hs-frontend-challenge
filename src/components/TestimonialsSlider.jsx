89cbc291b60563eaeeb33a086f34fa04e0838022
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-12-27 18:15:13 +0300

    Add sample testimonials

--- src/components/TestimonialsSlider.jsx
+++ src/components/TestimonialsSlider.jsx
@@ -1,6 +1,44 @@
 // TestimonialsSlider component
 
 const TestimonialsSlider = () => {
+  const testimonials = {
+    1: {
+      name: "Irene Pereyra",
+      title: "Research Lead, SCG",
+      testimonial:
+        "This Fellowship was a turning point in my career. I wouldn’t be where I am today without the financial support and experienced offered through the program.",
+      course: "B.A. Visual Design",
+      desgnation: "Education",
+      image: "src/assets/images/irene-pereyra.svg",
+    },
+    2: {
+      name: "Irene Pereyra",
+      title: "Research Lead, SCG",
+      testimonial:
+        "This Fellowship was a turning point in my career. I wouldn’t be where I am today without the financial support and experienced offered through the program.",
+      course: "B.A. Visual Design",
+      desgnation: "Education",
+      image: "src/assets/images/irene-pereyra.svg",
+    },
+    3: {
+      name: "Irene Pereyra",
+      title: "Research Lead, SCG",
+      testimonial:
+        "This Fellowship was a turning point in my career. I wouldn’t be where I am today without the financial support and experienced offered through the program.",
+      course: "B.A. Visual Design",
+      desgnation: "Education",
+      image: "src/assets/images/irene-pereyra.svg",
+    },
+    4: {
+      name: "Irene Pereyra",
+      title: "Research Lead, SCG",
+      testimonial:
+        "This Fellowship was a turning point in my career. I wouldn’t be where I am today without the financial support and experienced offered through the program.",
+      course: "B.A. Visual Design",
+      desgnation: "Education",
+      image: "src/assets/images/irene-pereyra.svg",
+    },
+  };
   return (
     <section className="testimonials-slider relative flex flex-col items-center px-[20px] bg-blue-300">
       <div className="h-[257px] w-full bg-[--color-primary] absolute top-0 left-0 z-10">
