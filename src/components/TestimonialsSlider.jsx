91eb0827dc063097b8c4d5421321478be548a768
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-12-27 18:13:51 +0300

    Refactor TestimonialsSlider component layout and styles for smll creen

--- src/components/TestimonialsSlider.jsx
+++ src/components/TestimonialsSlider.jsx
@@ -2,8 +2,21 @@
 
 const TestimonialsSlider = () => {
   return (
-    <section className="testimonials-slider bg-blue-700 text-white p-3">
-      <h2>Testimonials Slider</h2>
+    <section className="testimonials-slider relative flex flex-col items-center px-[20px] bg-blue-300">
+      <div className="h-[257px] w-full bg-[--color-primary] absolute top-0 left-0 z-10">
+        <div className="w-[292px] h-[257px] border border-[#6254BF] bg-repeat bg-[url('src/assets/images/testimonial-pattern.png')]"></div>
+      </div>
+      <div className="slider relative z-20 flex flex-col w-[335px] h-[456px] mt-[88px] border-lime-500 bg-orange-300">
+        {/* Your slider content goes here */}
+      </div>
+      <div className="controls relative z-20 w-full flex justify-end gap-[8px] mt-[24px]">
+        <button className="prev-btn w-[40px] h-[40px] border rounded-full">
+          prev
+        </button>
+        <button className="next-btn w-[40px] h-[40px] border rounded-full">
+          next
+        </button>
+      </div>
     </section>
   );
 };
