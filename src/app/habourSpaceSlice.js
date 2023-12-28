ae268d038512ff7fd182df542e37ede0e1c5a1ea
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-12-28 15:58:22 +0300

    Add habourSpaceSlice.js to handle fetching data from API

---
+++ src/app/habourSpaceSlice.js
@@ -0,0 +1,41 @@
+// habourSpaceSlice.js
+
+import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  data: {},
+  error: null,
+};
+
+export const fetchData = createAsyncThunk("habourSpace/fetchData", async () => {
+  const apiEndpoint =
+    "https://pre-prod.harbour.space/api/v1/scholarship_pages/data-science-apprenticeship-zeptolab";
+
+  const response = await fetch(apiEndpoint);
+  const data = await response.json();
+  return data;
+});
+
+export const habourSpaceSlice = createSlice({
+  name: "habourSpace",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchData.pending, (state) => {
+        state.error = null;
+      })
+      .addCase(fetchData.fulfilled, (state, action) => {
+        state.data = action.payload;
+        console.log(action.payload);
+      })
+      .addCase(fetchData.rejected, (state, action) => {
+        state.error = action.payload;
+      });
+  },
+});
+
+export const selectData = (state) => state.habourSpace.data;
+
+export default habourSpaceSlice.reducer;
