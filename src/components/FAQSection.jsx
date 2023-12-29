affe13b079df5786df8d4cf14b198ab95da43137
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-12-29 03:00:56 +0300

    Refactor FAQSection component to use dynamic data

--- src/components/FAQSection.jsx
+++ src/components/FAQSection.jsx
@@ -1,36 +1,38 @@
 // FAQSection component
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectData } from "../app/habourSpaceSlice";
 
 const FAQSection = () => {
-  const faq = {
-    1: {
-      question: "What are my obligations?",
-      answer:
-        "You will be required to work for 2 years at a company of your choice. We will help you find the right fit.",
-    },
-    2: {
-      question: "Do I get a job placement upon graduation?",
-      answer:
-        "You will be required to work for 2 years at a company of your choice. We will help you find the right fit.",
-    },
-    3: {
-      question: "What if I want to start my own company?",
-      answer:
-        "You will be required to work for 2 years at a company of your choice. We will help you find the right fit.",
-    },
-    4: {
-      question: "Do I need a visa?",
-      answer:
-        "You will be required to work for 2 years at a company of your choice. We will help you find the right fit.",
-    },
-  };
+  const data = useSelector(selectData);
 
-  // drop-down menu 
-  const [selectedOption, setSelectedOption] = useState('');
+  // section dynamic data
+  const faqs = data.scholarship.faqs.items;
+  const categories = data.scholarship.faqs.categories;
+
+  // drop-down menu
+  const [selectedOption, setSelectedOption] = useState("");
 
   const handleChange = (event) => {
     setSelectedOption(event.target.value);
   };
+
+  // State to track question visibility
+  const [visibleQuestions, setVisibleQuestions] = useState([]);
+
+  const toggleQuestion = (index) => {
+    // Toggle the visibility of the question at the given index
+    setVisibleQuestions((prevVisibleQuestions) => {
+      const updatedVisibility = [...prevVisibleQuestions];
+      updatedVisibility[index] = !updatedVisibility[index];
+      return updatedVisibility;
+    });
+  };
+
+  // Filter FAQs based on selected category
+  const filteredFaqs = selectedOption
+    ? faqs.filter((faq) => faq.type === selectedOption)
+    : faqs;
 
   return (
     <section className="faq-section px-[43px] pt-[70px] pb-[113px] md:pt-[170px] md:px-[142px] bg-purple-300">
@@ -45,32 +47,53 @@ const FAQSection = () => {
             onChange={handleChange}
             className="w-[284px] border border-lime-700 rounded-3xl px-[16px] py-[17px]"
           >
-            <option value="" className="text-[--color-primary]">Program</option>
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
-            <option value="option3">Option 3</option>
+            <option value="" className="text-[--color-primary]">
+              All
+            </option>
+            {categories.map((category) => {
+              return (
+                <option
+                  value={category}
+                  key={category}
+                  className="text-[--color-primary]"
+                >
+                  {category}
+                </option>
+              );
+            })}
           </select>
         </div>
       </div>
 
       <div className="mt-[40px] md:mt-[64px]">
         <hr className="border w-full mb-[32px] hidden md:block" />
-        {Object.keys(faq).map((key) => {
-          const item = faq[key];
-          return (
-            <React.Fragment key={key}>
-              <div className="flex justify-between items-center pb-[16px]">
-                <p>{item.question}</p>
-                <button className="w-[32px] h-[32px] border rounded-full">
-                  <p className="text-2xl">+</p>
-                </button>
+        {filteredFaqs.map((faq, index) => (
+          <React.Fragment key={index}>
+            <div className="flex justify-between pb-[16px]">
+              <p className="text-[--color-primary]">{faq.type}</p>
+              <div className="flex flex-col md:w-[300px] lg:w-[500px]">
+                <p className="font-bold">{faq.question}</p>
+                <p
+                  className={`mt-[20px] ${
+                    visibleQuestions[index] ? "" : "hidden"
+                  }`}
+                >
+                  {faq.answer[0].data}
+                </p>
               </div>
-              <hr className="border w-full mb-[18px] md:mb-[32px]" />
-            </React.Fragment>
-          );
-        })}
+              <button
+                className="w-[32px] h-[32px] border rounded-full"
+                onClick={() => toggleQuestion(index)}
+              >
+                <p className="text-2xl">
+                  {visibleQuestions[index] ? "-" : "+"}
+                </p>
+              </button>
+            </div>
+            <hr className="border w-full mb-[18px] md:mb-[32px]" />
+          </React.Fragment>
+        ))}
       </div>
-       
     </section>
   );
 };
